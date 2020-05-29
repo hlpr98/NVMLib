6daed103edcae78ffb5f4dcd6e4fc3c1d5a8b295
hlpr98 <hlpr98@gmail.com>
2020-05-29 17:42:06 +0530

    Updates

---
+++ navtreedata.js
@@ -0,0 +1,72 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "NVMLib", "index.html", [
+    [ "NVMLib", "index.html#autotoc_md9", [
+      [ "What the library is trying to achieve", "index.html#autotoc_md10", [
+        [ "How we achieve pointer validity and data integrity", "index.html#autotoc_md11", null ]
+      ] ],
+      [ "Unique things in the library", "index.html#autotoc_md12", null ],
+      [ "Compiling", "index.html#autotoc_md13", null ],
+      [ "Documentation", "index.html#autotoc_md14", null ],
+      [ "Dependecy graph", "index.html#autotoc_md15", null ]
+    ] ],
+    [ "Libraries used", "md__c_1__users_hlpr9__desktop__n_v_m_lib_src_c_new__r_e_a_d_m_e.html", [
+      [ "Things to be completed", "md__c_1__users_hlpr9__desktop__n_v_m_lib_src_c_new__r_e_a_d_m_e.html#autotoc_md4", null ]
+    ] ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"algo_8h.html",
+"malloc_8c.html#a988ea48df7c04c3d2c5afbdd376832f4",
+"structpool__free__slot__val__st.html#afb3b99084838a3689544c36dc65a6a16"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
